f39c6ab57756624161561e371957a6acc4486795
Justin Griswold <theeTGP@gmail.com>
2022-02-14 14:54:12 -0600

    chapter 11.6-11.9 completed

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -4,7 +4,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,21 +17,25 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    private static List<String> events = new ArrayList<>();
+
     @GetMapping
     public String displayEvents(Model model){
-        List<String> events = new ArrayList<>();
-        events.add("can You code this?");
-        events.add("I think, therefore I code ");
-        events.add("The happiness of your life depends on the quality of your code");
-        events.add("Believe you can code and you're halfway there");
         model.addAttribute("events", events);
-        return"events/index";
+        return "events/index";
     }
 
     //lives at/events/create
     @GetMapping("create")
     public String renderCreateEventForm(){
         return "events/create";
+    }
+
+    //lives at /events/create
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:";
     }
 
 }
